9cca8e8e01c2eed52bf3966a8f4685232fe1b9ed
Rafaela Borges <rafasborgesw@gmail.com>
2024-05-16 21:37:53 -0300

    Aula 19

--- Aula 19/Ex8.sql
+++ Aula 19/Ex8.sql
@@ -1,4 +1,3 @@
-INSERT INTO Medicos (codm, nome, idade, cidade, CPF, especialidade, nroa)
-SELECT F.codf, F.nome, F.idade, F.cidade, F.CPF, M.especialidade, M.nroa
-FROM Funcionarios F, Medicos M
-WHERE F.codf = 3 AND M.codm = 2;
+insert into medicos
+SELECT m.codm+1, f.nome, f.idade, f.cidade, f.cpf, (SELECT especialidade from medicos WHERE codm = 2), (SELECT nroa from medicos WHERE codm = 2) 
+from funcionarios f join medicos m on codf=3 and codm = (SELECT max(codm) from medicos)
